5c247ef0b631e9d6d701267c07015ec3d5abe22f
DongUk Kim <70856172+dongukkim-dev@users.noreply.github.com>
2023-11-07 17:43:55 +0900

    create project

---
+++ src/main/java/com/example/project/domain/Member.java
@@ -0,0 +1,29 @@
+package com.example.project.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+    private String password;
+    private String phone;
+
+}
